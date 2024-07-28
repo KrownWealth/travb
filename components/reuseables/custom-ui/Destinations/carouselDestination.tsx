7f33fa1cca684ef91c8fea3ebf71996c57c2ef7b
KrownWealth <abdulramonadeola36@gmail.com>
2024-07-28 21:40:13 +0100

    Added more images

--- components/reuseables/custom-ui/Destinations/carouselDestination.tsx
+++ components/reuseables/custom-ui/Destinations/carouselDestination.tsx
@@ -11,40 +11,27 @@ import '../../../../styles/autoplay-carousel.css'
 import { Card, CardContent } from '@/components/ui/card';
 import Link from 'next/link';
 import Image from 'next/image';
-// import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
-
-
 
 const AutoplayCarousel = () => {
 
   const destinations = [
-    {
-      title: "Explore Nature",
-      img: "/images/bird.svg",
-    },
-    {
-      title: "Explore Nature",
-      img: "/images/bird.svg",
-    },
-    {
-      title: "Explore Nature",
-      img: "/images/bird.svg",
-    },
-    {
-      title: "Explore Nature",
-      img: "/images/bird.svg",
-    },
-    {
-      title: "Explore Nature",
-      img: "/images/bird.svg",
-    },
+    { title: "Round The World Travel", img: "/images/round the world travel.jpg" },
+    { title: "Beach Vacation", img: "/images/beach-vacation.jpg" },
+    { title: "Island Vacation", img: "/images/island vacation.jpg" },
+    { title: "Moscow", img: "/images/moscow.jpg" },
+    { title: "Mexico", img: "/images/mexico-city.jpg" },
+    { title: "Sao-Paulo", img: "/images/sao-paulo.jpg" },
+    { title: "Tokyo", img: "/images/Tokyo.jpg" },
+    { title: "Delhi", img: "/images/delhi.jpg" },
+    { title: "Beijing", img: "/images/Beijing ancient china.jpg" },
+    { title: "Seoul", img: "/images/Seoul-South_Korea.jpg" },
+    { title: "Jakarta Indonesia", img: "/images/jakarta indonesia.jpg" }
   ];
+
   return (
     <section className='container my-20'>
       <div className="featuredDetinations my-10">
         <Swiper
-          // spaceBetween={30}
-          // slidesPerView={4}
           loop={true}
           autoplay={{
             delay: 0,
@@ -80,9 +67,7 @@ const AutoplayCarousel = () => {
           {destinations.map((destination) => (
             <SwiperSlide key={destination.title} className="swiper-slide">
               <Link href="#" className="group" prefetch={false}>
-                <Card
-                  className="relative flex overflow-hidden bg-[#222e39] h-[240px] text-white shadow-md items-center justify-center"
-                >
+                <Card className="relative overflow-hidden h-[240px] text-white shadow-md w-[350px] border-0">
                   <div className="absolute inset-0">
                     <Image
                       src={destination.img}
@@ -91,19 +76,18 @@ const AutoplayCarousel = () => {
                       style={{ objectFit: 'cover' }}
                       className="w-full h-full"
                     />
+                    <div className="absolute inset-0 bg-black opacity-10"></div>
                   </div>
                   <CardContent className="relative z-10 flex items-center justify-center h-full">
-                    <p className="text-white text-xl font-bold">{destination.title}</p>
+                    <p className="text-white text-2xl font-bold">{destination.title}</p>
                   </CardContent>
                 </Card>
               </Link>
-
             </SwiperSlide>
           ))}
         </Swiper>
       </div>
     </section>
-
   );
 }
 
